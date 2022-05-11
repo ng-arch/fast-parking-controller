0f06e67acb39351ddee9c7a2ca27c564d38f7d00
nestor-gaitan <nestor.gaitan@bowpi.com>
2022-05-10 22:44:34 -0600

    CREATE  - first version to client-parking

---
+++ src/main/java/com/umg/fast/FastParkingControllerApplication.java
@@ -0,0 +1,13 @@
+package com.umg.fast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastParkingControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastParkingControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/fast/FastParkingControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastParkingControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
