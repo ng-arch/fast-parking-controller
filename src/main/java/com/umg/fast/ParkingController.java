45311d728afbc33f6155e001962d47eb06cce1b6
nestor-gaitan <nestor.gaitan@bowpi.com>
2022-05-10 22:59:51 -0600

    CREATE  - first version to client-parking

---
+++ src/main/java/com/umg/fast/ParkingController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.umg.fast;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Nestor Gaitan
+ */
+@RestController
+public class ParkingController {
+    
+    @PostMapping("parking/controller/info")
+    public Map info(@RequestBody Map request){
+        Map response=new HashMap();
+        response.put("success", true);
+        System.out.println("controller info "+request);
+        return response;
+    }
+    
+}
